90b640af488f91679a490677090c984e99ccf98b
overwatcheddude <overwatcheddude@gmail.com>
2019-07-15 05:14:30 +0400

    First commit

---
+++ LinkExtractor.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+
+#Read the HTML document that you downloaded. The r is placed before filename to prevent the characters in filename string to be treated as special character.
+soup = BeautifulSoup(open(r"C:\Users\Overw\Downloads\Browse Items - Marketplace.tf.html"), "html.parser")
+
+#The text file will be called myLinks.txt
+#Write Only (‘w’) : Open the file for writing. For existing file, the data is truncated and over-written. The handle is positioned at the beginning of the file. Creates the file if the file does not exists.
+txtFile = open("myLinks.txt", "w")
+
+for link in soup.findAll("a"): #Finds all the <a> tags in the document
+    if link.get('href') is not None: #Gets the link (if the link is not #, empty, none or null)
+        txtFile.write(link.get('href') + "\n") #Writes the link into the text file and add a line break
+
+#Closes the file once the execution is complete.
+txtFile.close()
